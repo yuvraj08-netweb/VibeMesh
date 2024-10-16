75c6f7dcecb89b87bf7d6cf3ea9c05dc61f227f0
yuvraj08-netweb <yuvraj.netweb@gmail.com>
2024-10-16 13:37:16 +0530

    used emailjs to send automated replies when a user submits the ContactForm

--- src/components/Forms/ContactForm.jsx
+++ src/components/Forms/ContactForm.jsx
@@ -2,21 +2,24 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { Controller, useForm } from "react-hook-form";
 import Button from "../Common/Button";
-
+import emailjs from "emailjs-com";
+import { toast } from "react-toastify";
+import { useState } from "react";
 
 const ContactForm = () => {
 
-    const schema = yup.object().shape({
-        fullName: yup.string().required("Your Full Name is Required!"),
-        email: yup
-          .string()
-          .required("Email is required !")
-          .matches(
-            /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-            "Email is Invalid"
-          ),
-        message: yup.string().required("Please Enter Your Message!")
-      });
+  const schema = yup.object().shape({
+    fullName: yup.string().required("Your Full Name is Required!"),
+    email: yup
+      .string()
+      .required("Email is required !")
+      .matches(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+        "Email is Invalid"
+      ),
+    message: yup.string().required("Please Enter Your Message!"),
+  });
+
   const {
     control,
     handleSubmit,
@@ -32,16 +35,40 @@ const ContactForm = () => {
     },
   });
 
-  const submitForm = () =>{
+  const [loading,setLoading] = useState(false)
+
+  const sendEmail = async (data) => {
+    setLoading(true)
+    try {
+      const response = await emailjs.send(
+        import.meta.env.VITE_EMAIL_SERVICE_ID, // Replace with your EmailJS service ID
+        import.meta.env.VITE_EMAIL_CONTACT_TEMPLATE_ID, // Replace with your EmailJS template ID
+        data,
+        import.meta.env.VITE_EMAIL_PUBLIC_KEY // Replace with your EmailJS user ID
+      );
+      console.log("Email sent successfully:", response.status, response.text);
+      toast.success("Thank you for reaching out! We'll get back to you soon.");
+      reset();
+    } catch (error) {
+      console.error("Failed to send email:", error);
+      toast.error("Something went wrong. Please try again.");
+    }
+    setLoading(false)
+  };
+
+  const submitForm = (formData) => {
+    sendEmail(formData);
     reset();
-  }
+  };
   return (
     <form className="max-w-[450px]">
       <div className="formElement">
         <Controller
           name="fullName"
           control={control}
-          render={({ field }) => <input {...field} placeholder="Enter Full Name" />}
+          render={({ field }) => (
+            <input {...field} placeholder="Enter Full Name" />
+          )}
         />
         <p className="errorPara">{errors.fullName?.message}</p>
       </div>
@@ -57,15 +84,26 @@ const ContactForm = () => {
         <Controller
           name="message"
           control={control}
-          render={({ field }) => <textarea {...field} placeholder="Your message..." />}
+          render={({ field }) => (
+            <textarea {...field} placeholder="Your message..." />
+          )}
         />
         <p className="errorPara">{errors.message?.message}</p>
       </div>
-      
-      <div className="formElement">
-      <Button btnText={"Submit"} btnFun={handleSubmit(submitForm)} className="bg-darkPurple text-lightPurple"/>
-      </div>
 
+      <div className="formElement">
+        <Button
+          btnText={
+            loading ? 
+            <>
+              <span>Sending <i className="fa fa-spinner"></i></span>
+            </> :  
+            "Submit"
+          }
+          btnFun={handleSubmit(submitForm)}
+          className="bg-darkPurple text-lightPurple"
+        />
+      </div>
     </form>
   );
 };
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,31 +1,18 @@
-/* eslint-disable no-unused-vars */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_API_KEY,
-//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_APP_ID,
-//   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
-// };
-
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAYZmktUy8bHyvmQLsUjsnM-z6puzSPjqE",
-  authDomain: "vibemesh08.firebaseapp.com",
-  projectId: "vibemesh08",
-  storageBucket: "vibemesh08.appspot.com",
-  messagingSenderId: "227472705507",
-  appId: "1:227472705507:web:912c7468983e1af3f742d6",
-  measurementId: "G-F1LDCFJWZS"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
